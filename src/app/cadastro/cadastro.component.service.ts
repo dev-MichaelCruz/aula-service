9c163690002d69e60aff08c445c1ed6fa7e431e9
dev-MichaelCruz <michaelcruz.dev@gmail.com>
2023-11-01 12:57:49 -0300

    Adicionadas funcionalidades

--- src/app/cadastro/cadastro.component.service.ts
+++ src/app/cadastro/cadastro.component.service.ts
@@ -1,3 +1,18 @@
+import { Livro } from "./ILivro";
+
 export class CadastroService {
-    tarefa = ["Estudar", "Trabalhar", "Comer"]
+    livros: Livro[] = [];
+
+    addLivro(titulo: string, autor: string, anoPublicacao: string) {
+        const novoLivro: Livro = {
+            titulo,
+            autor,
+            anoPublicacao
+        };
+        this.livros.push(novoLivro);
+    }
+
+    removeLivro(index: number) {
+        this.livros.splice(index, 1);
+    }
 }
--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,17 +1,37 @@
 import { Component } from '@angular/core';
 import { CadastroService } from './cadastro.component.service';
+import { Livro } from './ILivro';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.scss']
 })
+
 export class CadastroComponent {
-  tarefas!: string;
-  listaTarefas!: string[];
+  listaLivros!: Livro[];
+
+  titulo!: string
+  autor!: string
+  anoPublicacao!: string
 
   constructor(private cadastroService: CadastroService) {
-    this.listaTarefas = this.cadastroService.tarefa;
+    this.listaLivros = this.cadastroService.livros;
+  }
+
+  addLivro() {
+    this.cadastroService.addLivro(this.titulo, this.autor, this.anoPublicacao)
+    this.clearDados();
+  }
+
+  removeLivro(index: number) {
+    this.cadastroService.removeLivro(index)
+  }
+
+  clearDados() {
+    this.titulo = "";
+    this.autor = "";
+    this.anoPublicacao = "";
   }
 
 }
