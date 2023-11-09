5c442dde35cebea3128160333732910ebc34697f
Michael Cruz <michael.cruz@ibm.com>
2023-11-09 09:24:17 -0300

    Atualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app.routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
-import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
-import { CadastroService } from './components/cadastro/cadastro.component.service';
-import { AppRoutingModule } from './app.routing.module';
 import { ListarComponent } from './components/listar/listar.component';
 import { LogsComponent } from './components/logs/logs.component';
+
+import { CadastroService } from './components/cadastro/cadastro.component.service';
 import { LogService } from './components/logs/logs.service';
-=======
->>>>>>> b6e2b6dc3a2c85589c709490a8b3ccf09e615fd5
 
 @NgModule({
   declarations: [
@@ -25,11 +23,7 @@ import { LogService } from './components/logs/logs.service';
     FormsModule,
     AppRoutingModule,
   ],
-<<<<<<< HEAD
   providers: [CadastroService, LogService],
-=======
-  providers: [],
->>>>>>> b6e2b6dc3a2c85589c709490a8b3ccf09e615fd5
   bootstrap: [AppComponent]
 })
 export class AppModule { }
